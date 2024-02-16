af00484b43b93103368dcdc6803b89213dd050ab
fredsu <fredsu@stud.ntnu.no>
2024-02-16 19:01:23 +0100

    add EncodeData method in apiUtility

--- backend/internal/utility/apiUtility/apiUtility.go
+++ backend/internal/utility/apiUtility/apiUtility.go
@@ -1,10 +1,13 @@
 package apiUtility
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
 )
 
+// ValidateURL checks if the url is longer then the given minimum length.
+// If the url is not valid, it returns false.
 func ValidateURL(w http.ResponseWriter, r *http.Request, maxLength int) bool {
 	pathList := strings.Split(r.URL.Path, "/")
 	if len(pathList) < maxLength {
@@ -12,4 +15,12 @@ func ValidateURL(w http.ResponseWriter, r *http.Request, maxLength int) bool {
 		return false
 	}
 	return true
+}
+
+// EncodeData encodes a struct to json and writes it to the response writer. It returns an error if the encoding fails.
+func EncodeData(w http.ResponseWriter, data interface{}) error {
+	w.Header().Add("content-type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "\t")
+	return encoder.Encode(data)
 }
