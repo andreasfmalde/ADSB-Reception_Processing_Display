2f15a5e5d4d9ff3d06c910e6acf2f29940c65e8c
fredsu <fredsu@stud.ntnu.no>
2024-02-17 15:28:38 +0100

    made EncodeData handle error

--- backend/internal/utility/apiUtility/apiUtility.go
+++ backend/internal/utility/apiUtility/apiUtility.go
@@ -1,6 +1,7 @@
 package apiUtility
 
 import (
+	"adsb-api/internal/logger"
 	"encoding/json"
 	"net/http"
 	"path"
@@ -19,9 +20,13 @@ func ValidateURL(w http.ResponseWriter, r *http.Request, maxLength int) bool {
 }
 
 // EncodeData encodes a struct to json and writes it to the response writer. It returns an error if the encoding fails.
-func EncodeData(w http.ResponseWriter, data interface{}) error {
+func EncodeData(w http.ResponseWriter, data interface{}) {
 	w.Header().Add("content-type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.SetIndent("", "\t")
-	return encoder.Encode(data)
+	err := encoder.Encode(data)
+	if err != nil {
+		http.Error(w, "Failed to encode data", http.StatusInternalServerError)
+		logger.Error.Println("Failed to encode data: %q", err)
+	}
 }
