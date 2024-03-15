e57d5e67a052b0f3cf0a13121857da1e70d4b2b3
Andreas Follevaag Malde <80474168+andreasfmalde@users.noreply.github.com>
2024-03-15 13:51:29 +0100

    Updated database functionality (#32)
    * add: struct representing an aircraft record
    
    * add: database initialization
    
    * add: implemented method that will create a current_time_aircraft table
    
    * add: method that will update the current time aircrafts table
    
    * add: method to retrieve all current aircrafts
    
    * add: date and time string convertion to valid timestamp
    
    * add: TCP connection and SBS processing functions implemented
    
    * add: connect to adsbhub and process SBS into database
    
    * refactor: make a new tcp connection each time trying to recieve SBS data
    
    * add: implement looping functionality to load new sbs data into database
    
    * update: update primary key to include icao and timestamp
    
    * update: insert new aircrafts using one single sql statement
    
    * add: delete outdated rows in current_time table
    
    * add: delete rows every 2 minutes in the current_time table
    
    * add: proper commenting on new implementation
    
    * update: add node_modules to gitignore
    
    * add: add a global constant for adsbhub waiting time
    
    * update: new query string for retrieving the latest current aircrafts

--- backend/internal/db/database.go
+++ backend/internal/db/database.go
@@ -8,9 +8,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-/*
-Initialize the PostgreSQL database and return the connection pointer
-*/
+// Initialize the PostgreSQL database and return the connection pointer
 func InitDatabase() (*sql.DB, error) {
 
 	dbLogin := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
@@ -20,19 +18,21 @@ func InitDatabase() (*sql.DB, error) {
 
 }
 
-/*
-Close the connection to the database
-*/
+// Close the connection to the database
 func CloseDatabase(db *sql.DB) error {
 	return db.Close()
 }
 
-/*
-Create current_time_aircraft table in database if it does not already exists
-*/
+// Create current_time_aircraft table in database if it does not already exists
 func CreateCurrentTimeAircraftTable(db *sql.DB) error {
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS current_time_aircraft(" +
-		"icao VARCHAR(6) NOT NULL PRIMARY KEY ," +
+	// Begin a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	// Create current_time table
+	_, err = tx.Exec("CREATE TABLE IF NOT EXISTS current_time_aircraft(" +
+		"icao VARCHAR(6) NOT NULL," +
 		"callsign VARCHAR(10) NOT NULL," +
 		"altitude INT NOT NULL," +
 		"lat DECIMAL NOT NULL," +
@@ -40,44 +40,79 @@ func CreateCurrentTimeAircraftTable(db *sql.DB) error {
 		"speed INT NOT NULL," +
 		"track INT NOT NULL," +
 		"vspeed INT NOT NULL," +
-		"timestamp TIMESTAMP NOT NULL);")
+		"timestamp TIMESTAMP NOT NULL," +
+		"PRIMARY KEY(icao,timestamp	));")
 
-	return err
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	// Create another index on the timestamp column
+	_, err = tx.Exec("CREATE INDEX IF NOT EXISTS timestamp_index ON current_time_aircraft(timestamp);")
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	// Commit the transaction
+	return tx.Commit()
 }
 
-/*
-Update the current_time_aircraft table with the new aircraft records provided from
-the parameter 'aircrafts'
-*/
+// Update the current_time_aircraft table with the new aircraft records provided from
+// the parameter 'aircrafts'
 func UpdateCurrentAircraftsTable(db *sql.DB, aircrafts []global.Aircraft) error {
-	// Delete the current table
-	if _, err := db.Exec("DROP TABLE current_time_aircraft"); err != nil {
-		return err
-	}
-	// Create a new current_time_aircraft table
-	if err := CreateCurrentTimeAircraftTable(db); err != nil {
-		return err
-	}
-	// Fill the new current_time_aircraft table
+
+	query := "INSERT INTO current_time_aircraft VALUES "
+
 	for _, aircraft := range aircrafts {
-		_, err := db.Exec("INSERT INTO current_time_aircraft VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)",
+		entry := fmt.Sprintf("('%s','%s','%d','%f','%f','%d','%d','%d','%s'),",
 			aircraft.Icao, aircraft.Callsign, aircraft.Altitude, aircraft.Latitude, aircraft.Longitude,
 			aircraft.Speed, aircraft.Track, aircraft.VerticalRate, aircraft.Timestamp)
-		if err != nil {
-			return err
-		}
+		query = query + entry
 	}
+	if query[len(query)-1] == ',' {
+		query = query[:len(query)-1]
+	}
+	query = query + ";"
+
+	_, err := db.Exec(query)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
+
 }
 
-/*
-Method to retrieve a list of all current aircrafts in the
-current_time_aircraft table
-*/
+// Method that will delete rows older that 6 seconds
+// from the lastest entry.
+func DeleteCurrentTimeAircrafts(db *sql.DB) error {
+	// Begin transaction
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	// Delete all rows older than 6 second from the latest entry
+	_, err = tx.Exec("DELETE FROM current_time_aircraft where timestamp" +
+		" < (select max(timestamp)-(6 * interval '1 second')" +
+		" from current_time_aircraft);")
+	// Roll back transaction if failed
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	// Commit transaction
+	return tx.Commit()
+
+}
+
+// Method to retrieve a list of all current aircrafts in the
+// current_time_aircraft table
 func RetrieveCurrentTimeAircrafts(db *sql.DB) ([]global.Aircraft, error) {
 	var aircrafts []global.Aircraft
 	// Make the query to the database
-	rows, err := db.Query("SELECT * FROM current_time_aircraft")
+	rows, err := db.Query("select * from current_time_aircraft where timestamp > " +
+		"(select max(timestamp)-(6 * interval '1 second') from current_time_aircraft);")
 	if err != nil {
 		return []global.Aircraft{}, err
 	}
