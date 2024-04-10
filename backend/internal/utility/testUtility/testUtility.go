2647149d65eb880ca7347e34bd6bc100d9a05d7e
Fredrik Sundt-Hansen <38140980+FredrikSundt-Hansen@users.noreply.github.com>
2024-04-10 11:34:46 +0200

    History filter by timestamp (#64)
    * create docker compose that builds backend with postgres db and successfully receive data from AdsbHub
    
    * DB_HOST=postgres_database in compose.yml to make adsb_container connect to postgres_container
    
    Change name of global database variables Linting in env.go
    
    * add .env through volumes Cleaner Dockerfile
    
    * remove unused databaseMock.go
    
    * merge compose files of backend and frontend into one
    
    Delete frontend compose.yml
    
    * merge compose files of backend and frontend into one
    
    Delete frontend compose.yml
    
    * add compose for fontend back If the user only wants to deploy web
    
    * update dockerignore
    
    * Fix, frontend now works with backend in docker
    
    * new fix frontend should work backend in docker compose
    
    * remove git install from docker and change to golang:latest
    
    * new sbs test for empty line
    
    * add healthcheck to postgres_container and backend_container
    
    Did not work to add healthcheck on frontend_container
    
    remove empty line backend.sh
    
    * create method for retrieving history by timestamp filter
    
    * add t testing.T to setuo and teardown
    
    If there is an error, they will tell which test did fail
    
    * add missing comments Rename Database to PgDatabase
    
    Clearly states that this is a postgres database interface
    
    * change CreateAircraftCurrentTimestampIndex to CreateAircraftHistoryTimestampIndex
    
    method will now create an index on history table instead of current table due to timestamp is not used for data retrieval anymore Change sbs service method to support this
    
    change interface name to Database
    
    * remove DB_PORT environment variable
    
    Not needed since it has to be 5432 for the postgres:latest image
    
    * change 'var query' to 'query :='
    
    go standard:
    - var for package level variables
    - := for local variables
    
    * alter DeleteOldHistory to delete history older than max timestamp
    
    * add SelectAllColumnHistoryByIcaoFilterByTimestamp to interface and add the equivalent method in restService.go
    
    * when validating url used len(url) instead of len(strings.Split(url, "/")
    
    * linting in apiUtility.go
    
    * add hour query parameter to history endpoint
    
    change history endpoint to accept icao not as a query parameter but has the last element of the path
    
    * add check for coordinates more than two to method HistoryModelToGeoJson
    
    * rename params to optionalParams for clarity
    
    * split docker file in backend to two for each main file
    
    Refactor global.env Adds three new methods, two for initializing env variables for reception api and rest api, third one for checking the variables and give default values or stop the program if the variable is required (db_user, db_password, sbs_source)
    
    * fix TestAdsbDB_DeleteOldHistory from merging
    
    * remove timestamp from aircraft_current primary key
    
    * linting in database.go
    
    * add: filter by timestamp based on latest aircraft in database
    
    * add: small changes in error handling in InitDB
    
    ---------
    
    Co-authored-by: Andreas Follevaag Malde <andrefma@stud.ntnu.no>

--- backend/internal/utility/testUtility/testUtility.go
+++ backend/internal/utility/testUtility/testUtility.go
@@ -27,6 +27,20 @@ func CreateMockAircraft(n int) []models.AircraftCurrentModel {
 	return aircraft
 }
 
+func CreateMockAircraftWithTimestamp(icao string, timestamp string) models.AircraftCurrentModel {
+	return models.AircraftCurrentModel{
+		Icao:         icao,
+		Callsign:     "TEST",
+		Altitude:     10000,
+		Latitude:     51.5074,
+		Longitude:    0.1278,
+		Speed:        450,
+		Track:        180,
+		VerticalRate: 0,
+		Timestamp:    timestamp,
+	}
+}
+
 func CreateMockHistAircraft(n int) []models.AircraftHistoryModel {
 	var aircraft []models.AircraftHistoryModel
 
@@ -43,34 +57,15 @@ func CreateMockHistAircraft(n int) []models.AircraftHistoryModel {
 	return aircraft
 }
 
-func CreateMockAircraftWithTimestamp(icao string, timestamp string) models.AircraftCurrentModel {
-	return models.AircraftCurrentModel{
-		Icao:         icao,
-		Callsign:     "TEST",
-		Altitude:     10000,
-		Latitude:     51.5074,
-		Longitude:    0.1278,
-		Speed:        450,
-		Track:        180,
-		VerticalRate: 0,
-		Timestamp:    timestamp,
-	}
-}
-
-func CreateMockAircraftWithIcao(n int, icao string) []models.AircraftCurrentModel {
-	var aircraft []models.AircraftCurrentModel
+func CreateMockHistAircraftWithIcao(n int, icao string) []models.AircraftHistoryModel {
+	var aircraft []models.AircraftHistoryModel
 
 	for i := 0; i < n; i++ {
-		ac := models.AircraftCurrentModel{
-			Icao:         icao,
-			Callsign:     strconv.Itoa(i),
-			Altitude:     i,
-			Latitude:     float32(i),
-			Longitude:    float32(i),
-			Speed:        i,
-			Track:        i,
-			VerticalRate: i,
-			Timestamp:    time.Now().Add(time.Duration(i) * time.Second).Format(time.DateTime),
+		ac := models.AircraftHistoryModel{
+			Icao:      icao,
+			Latitude:  float32(i),
+			Longitude: float32(i),
+			Timestamp: time.Now().Add(time.Duration(i) * time.Second).Format(time.DateTime),
 		}
 		aircraft = append(aircraft, ac)
 	}
